640d3fb1a9f9a4195433d0718c20ff972398a775
Fitzfoufou <francois.fitzpatrick@gmail.com>
2022-10-20 10:28:16 -0400

    feat(Models): add models

---
+++ app/models/account.rb
@@ -0,0 +1,5 @@
+class Account < ApplicationRecord
+    belongs_to :user
+    has_many :credited_transactions, :class_name => "Transaction", :foreign_key => 'debit_account_id'
+    has_many :debited_transactions, :class_name => "Transaction", :foreign_key => 'credit_account_id'
+end
