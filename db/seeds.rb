c192aa1cba32793704f8d9c03b65986e62ccf480
Fitzfoufou <francois.fitzpatrick@gmail.com>
2022-10-20 10:28:47 -0400

    feat(seeds): add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+asterix = User.create!(
+  name: 'Astérix'
+)
+
+obelix = User.create!(
+  name: 'Obélix'
+)
+
+asterix_checking_account = Account.create!(
+  name: 'Checking account',
+  initial_amount: 0.00,
+  user: asterix
+)
+asterix_saving_account = Account.create!(
+  name: 'Saving account',
+  initial_amount: 0.00,
+  user: asterix
+)
+obelix_checking_account = Account.create!(
+  name: 'Checking account',
+  initial_amount: 5000.00,
+  user: obelix
+)
+
+Transaction.create!(
+  [
+    {
+      debit_account: obelix_checking_account,
+      credit_account: asterix_checking_account,
+      amount: 2500.00,
+      transfer_date: DateTime.new(2022, 10, 10, 0, 0, 0)
+    },
+    {
+      debit_account: asterix_checking_account,
+      credit_account: asterix_saving_account,
+      amount: 2100.00,
+      transfer_date: DateTime.new(2022, 10, 12, 0, 0, 0)
+    }
+  ]
+)
