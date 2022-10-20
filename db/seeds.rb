3a969df0e3b600e0908586a478c59227c724755e
Fitzfoufou <francois.fitzpatrick@gmail.com>
2022-10-20 10:28:47 -0400

    feat(seeds): add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+asterix = User.create!(
+    name: "Astérix"
+)
+
+obelix = User.create!(
+    name: "Obélix"
+)
+  
+asterix_checking_account = Account.create!(
+    name: "Checking account",
+    current_amount: 0.00,
+    user: asterix
+)
+asterix_saving_account = Account.create!(
+    name: "Saving account",
+    current_amount: 0.00,
+    user: asterix
+)
+obelix_checking_account = Account.create!(
+    name: "Checking account",
+    current_amount: 5000.00,
+    user: obelix
+)
+
+Transaction.create!(
+    [
+        {
+            debit_account: obelix_checking_account,
+            credit_account: asterix_checking_account, 
+            amount: 2500.00,
+        },
+        {
+            debit_account: asterix_checking_account,
+            credit_account: asterix_saving_account, 
+            amount: 2100.00,
+        }
+    ]
+)
