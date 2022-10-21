41b2d82bb5a351e652946b0327141f1a6d90dd98
Fitzfoufou <francois.fitzpatrick@gmail.com>
2022-10-21 17:57:08 -0400

    feat(account_type.rb): add current_amount to graphql object

--- app/graphql/types/account_type.rb
+++ app/graphql/types/account_type.rb
@@ -5,9 +5,14 @@ module Types
     field :id, ID, null: false
     field :name, String
     field :initial_amount, Float
+    field :current_amount, Float
     field :user_id, Integer
     field :user, Types::UserType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def current_amount
+      object.current_amount
+    end
   end
 end
