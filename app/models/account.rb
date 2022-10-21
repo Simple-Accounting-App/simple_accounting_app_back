ff37deb386a5cfc7bd40f453015a9f0f8453549f
Fitzfoufou <francois.fitzpatrick@gmail.com>
2022-10-21 17:56:43 -0400

    feat(account.rb): add and test current_amount method

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,12 @@
 class Account < ApplicationRecord
   belongs_to :user
-  has_many :credited_transactions, class_name: 'Transaction', foreign_key: 'debit_account_id'
-  has_many :debited_transactions, class_name: 'Transaction', foreign_key: 'credit_account_id'
+  has_many :credited_transactions, class_name: 'Transaction', foreign_key: 'credit_account_id'
+  has_many :debited_transactions, class_name: 'Transaction', foreign_key: 'debit_account_id'
+
+  def current_amount
+    credited_transactions_amount = credited_transactions.where('transfer_date <= ?', Time.now).sum(:amount)
+    debited_transactions_amount = debited_transactions.where('transfer_date <= ?', Time.now).sum(:amount)
+
+    initial_amount + credited_transactions_amount - debited_transactions_amount
+  end
 end
